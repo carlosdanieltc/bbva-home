da397a06eb517cd6f58303acb6f74ed4fc7962a3
Carlos <carlosdanieltc@gmail.com>
2023-05-08 16:34:06 +0200

    docs

--- rollup.config.js
+++ rollup.config.js
@@ -14,7 +14,7 @@ export default {
     babel({ babelHelpers: "bundled" }),
     terser(),
     copy({
-      targets: [{ src: "public/*", dest: "dist" }],
+      targets: [{ src: "public/*", dest: "dist" }], 
     }),
   ],
   watch: {
